f298c9fc3dca00e755bbf6f5daa8cb504b44d425
Thomas Ottaway <tottaway123@gmail.com>
2018-09-21 00:01:34 -0400

    Experimenting with a lighter theme

--- mysite/coop/views.py
+++ mysite/coop/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'coop/index.html')
+    return render(request, 'coop/base.html')
+
+def dark_index(request):
+    return render(request, 'coop/base2.html')
