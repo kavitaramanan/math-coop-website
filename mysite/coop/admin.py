c00172f95dbab173da7368fdefd8aba6d37905cc
Thomas Ottaway <tottaway123@gmail.com>
2018-09-24 12:14:22 -0400

    Beginnings of the database

--- mysite/coop/admin.py
+++ mysite/coop/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from coop.models import *
+
 # Register your models here.
+class PresentationFileAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+class PresentationAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(PresentationFile, PresentationFileAdmin)
+admin.site.register(Presentation, PresentationAdmin)
+admin.site.register(Topic, TopicAdmin)
--- mysite/coop/models.py
+++ mysite/coop/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class PresentationFile(models.Model):
+    specifications = models.FileField(upload_to='ppts/')
+    name = models.CharField(max_length=200)
+
+class Presentation(models.Model):
+    name = models.CharField("Name", max_length=200)
+    file_dir = models.CharField("File", max_length=200)
+    summary = models.CharField("Summary", max_length=1000)
+    author = models.CharField("Author", max_length=50)
+    topics = models.CharField("Topics", max_length = 200)
+
+
+class Topic(models.Model):
+    name = models.CharField("Topic", max_length=50)
