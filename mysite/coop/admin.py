82f05c47d2df6de7ac360ad64003ebae3f95ca7e
Thomas Ottaway <tottaway123@gmail.com>
2018-12-02 14:58:52 -0500

    Switch to using admin page (should have done this a LONG time ago)

--- mysite/coop/admin.py
+++ mysite/coop/admin.py
@@ -5,18 +5,23 @@ from .models import *
 class TopicAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Topic, TopicAdmin)
+
 class PresentationAdmin(admin.ModelAdmin):
-    pass
+    filter_horizontal = ("topics",) 
 admin.site.register(Presentation, PresentationAdmin)
+
 class FileAdmin(admin.ModelAdmin):
     pass
 admin.site.register(File, FileAdmin)
+
 class PersonAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Person, PersonAdmin)
+
 class OutreachAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Outreach, OutreachAdmin)
-class PresentationTopicAdmin(admin.ModelAdmin):
-    pass
-admin.site.register(PresentationTopic, PresentationTopicAdmin)
+
+# class PresentationTopicAdmin(admin.ModelAdmin):
+#     pass
+# admin.site.register(PresentationTopic, PresentationTopicAdmin)
--- mysite/coop/models.py
+++ mysite/coop/models.py
@@ -9,6 +9,7 @@ class Presentation(models.Model):
     summary = models.CharField("Summary", max_length=1000)
     author = models.CharField("Author", max_length=50)
     level = models.CharField("Level", max_length=200)
+    topics = models.ManyToManyField(Topic, related_name="pres")
 
 class File(models.Model):
     pres = models.ForeignKey(Presentation, on_delete=models.CASCADE)
@@ -18,18 +19,21 @@ class Person(models.Model):
     name = models.CharField("Name", max_length=50, primary_key=True)
     image = models.ImageField(upload_to="images/")
     bio = models.CharField("Biography", max_length=1000)
+    class Meta:
+        verbose_name_plural = "People"
 
 class Outreach(models.Model):
     name = models.CharField("Name", max_length=100, primary_key=True)
     location = models.CharField("Location", max_length=200)
     date = models.DateField("Date")
     description = models.CharField("Description", max_length=1000)
+    people = models.ManyToManyField(Person, related_name="outreach")
 
-class PersonOutreach(models.Model):
-    person = models.ForeignKey(Person, on_delete=models.CASCADE)
-    outreach = models.ForeignKey(Outreach, on_delete=models.CASCADE)
+# class PersonOutreach(models.Model):
+#     person = models.ForeignKey(Person, on_delete=models.CASCADE)
+#     outreach = models.ForeignKey(Outreach, on_delete=models.CASCADE)
 
-class PresentationTopic(models.Model):
-    presentation = models.ForeignKey(Presentation, on_delete=models.CASCADE)
-    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+# class PresentationTopic(models.Model):
+#     presentation = models.ForeignKey(Presentation, on_delete=models.CASCADE)
+#     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
  
